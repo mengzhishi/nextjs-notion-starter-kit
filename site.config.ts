961cdb64a05f72f44f98fd280a0ce8ee9dfb374f
nangder <44994326+mengzhishi@users.noreply.github.com>
2022-05-19 21:21:40 +0800

    Update site.config.ts
    set self information firstly

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Blog-9c12bc9756fa4ee8be3f5d89677a33c7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'cthumio',
+  domain: 'https://cthumio.notion.site/cthumio/Blog-9c12bc9756fa4ee8be3f5d89677a33c7',
+  author: 'cthumio',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'cthumio',
+  github: 'cthumio',
+  linkedin: 'cthumio',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
